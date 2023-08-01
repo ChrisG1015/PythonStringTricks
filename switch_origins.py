a79200dcfec73c244a0292b6568f17f0d700f965
ChrisG1015 <45828554+ChrisG1015@users.noreply.github.com>
2023-08-01 15:18:17 -0400

    Update switch_origins.py

--- switch_origins.py
+++ switch_origins.py
@@ -13,7 +13,31 @@ def get_cloudfront_distribution(distribution_id, profile_name):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+def get_cloudfront_primary_origin(distribution_id, profile_name):
+    # Create a Boto3 CloudFront client
+    session = boto3.Session(profile_name=profile_name)
+    client = session.client('cloudfront')
 
+    try:
+        # Get the distribution configuration
+        response = client.get_distribution(Id=distribution_id)
+        distribution_config = response['Distribution']['DistributionConfig']
+
+        # Check if the distribution has origins and retrieve the primary origin domain
+        if 'Origins' in distribution_config and 'Items' in distribution_config['Origins']:
+            primary_origin_domain = distribution_config['Origins']['Items'][0]['DomainName']
+            return primary_origin_domain
+
+        # Return None if no origins are found
+        return None
+
+    except client.exceptions.NoSuchDistribution:
+        print(f"CloudFront distribution with ID '{distribution_id}' not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+        
 def switch_primary_origin(distribution, profile_name, new_flag):
     if 'Origins' in distribution and 'Items' in distribution['Origins']:
         primary_origin = distribution['Origins']['Items'][0]
