291fbf91506590536fc85cdc6917dcbe510f5f78
ChrisG1015 <45828554+ChrisG1015@users.noreply.github.com>
2023-09-12 11:58:47 -0400

    Update bash_scripts.sh

--- bash_scripts.sh
+++ bash_scripts.sh
@@ -1,4 +1,7 @@
 prefix=$(echo "$url" | sed -n 's|\(https://.*amazon\.aws\.com\).*|\1|p')
 suffix=$(echo "$url" | sed -n 's|https://.*amazon\.aws\.com\(/out/.*\)|\1|p')
 aws lambda list-layers --query "Layers[?LayerName=='YourLayerName'].LayerArn" --output text
+ "Layers": [
+          { "Fn::ImportValue": { "Fn::Sub": "${LayerArnParameter}" } }
+        ]
 
