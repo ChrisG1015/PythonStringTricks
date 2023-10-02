73ff49ca39e3d5bc5c779ee61223c0d13a3eab7f
ChrisG1015 <45828554+ChrisG1015@users.noreply.github.com>
2023-10-02 14:13:39 -0400

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
