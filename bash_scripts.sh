6f9895ef58bf04f028b668babdc6eac3b89372d4
ChrisG1015 <45828554+ChrisG1015@users.noreply.github.com>
2023-10-24 20:02:14 -0400

    Update bash_scripts.sh

--- bash_scripts.sh
+++ bash_scripts.sh
@@ -7,4 +7,6 @@ aws lambda list-layers --query "Layers[?LayerName=='YourLayerName'].LayerArn" --
 
 
 aws events put-rule --name "UpdateDistributionRule" --event-pattern "{\"source\": [\"aws.cloudtrail\"], \"detail\":{\"eventName\":[\"UpdateDistribution\"]}}"
+aws cloudwatch put-metric-alarm --alarm-name "fast-UpdateDistributionAlarm" --alarm-description "fast-UpdateDistribution event detected" --actions-enabled --metric-name "EventCount" --namespace "AWS/Events" --statistic "SampleCount" --period 60 --threshold 1 --comparison-operator "GreaterThanOrEqualToThreshold" --evaluation-periods 1 --dimensions Name=RuleName,Value="fast-UpdateDistributionRule"
+
 
