abb8131b90f0293b690d8b71ddef3b83e6222253
ChrisG1015 <45828554+ChrisG1015@users.noreply.github.com>
2024-01-12 19:00:53 -0500

    Update sec_group_lambda.tf

--- sec_group_lambda.tf
+++ sec_group_lambda.tf
@@ -1,112 +1,78 @@
+variable "sec_group_name" {
+  description = "Name of the security group"
+}
+
+variable "ports" {
+  description = "Map of ports to create security groups"
+}
+
+resource "aws_security_group" "dynamic_sec_group" {
+  count        = length(keys(var.ports))
+  name         = "${var.sec_group_name}-${element(keys(var.ports), count.index)}"
+  description  = "Dynamic Security Group for ${element(keys(var.ports), count.index)}"
+  
+  dynamic "ingress" {
+    for_each = var.ports
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+output "sec_group_ids" {
+  value = aws_security_group.dynamic_sec_group[*].id
+}
+
+
+
+
 provider "aws" {
-  region = "us-east-1"  # Change this to your desired AWS region
+  region = "us-east-1"  # Update with your desired region
 }
 
-# IAM Role
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      }
-    }
-  ]
-}
-EOF
-}
-
-# IAM Policy for Lambda Execution Role
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_policy"
-  description = "Policy for Lambda function"
+module "cyberdynamic_api" {
+  source = "./modules/cyberdynamic_api"
   
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetObject",
-        "s3:PutObject"
-      ],
-      "Resource": "arn:aws:s3:::your-bucket-name/*"  # Replace with your S3 bucket ARN
+  sec_group_name = "cyberdynamic"
+  ports = {
+    "https" = 443,
+    "rdp"   = 3389
+    # Add more ports as needed
+  }
+}
+
+# Save security group IDs to a YAML file
+locals {
+  security_group_ids = {
+    "https-cyberdynamic-api" = {
+      ports         = [443],
+      sec_group_id  = module.cyberdynamic_api.sec_group_ids[0]  # Assuming https is the first port
     },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:DescribeInstances"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+    "rdp-cyberdynamic-api" = {
+      ports         = [3389],
+      sec_group_id  = module.cyberdynamic_api.sec_group_ids[1]  # Assuming rdp is the second port
+    },
+    # Add more entries for other ports
+  }
 }
 
-# Attach Lambda Policy to IAM Role
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  policy_arn = aws_iam_policy.lambda_policy.arn
-  role       = aws_iam_role.lambda_role.name
+resource "local_file" "yaml_file" {
+  content  = yamlencode(local.security_group_ids)
+  filename = "security_groups.yaml"
 }
 
-# Security Group
-resource "aws_security_group" "lambda_sg" {
-  name        = "lambda_sg"
-  description = "Lambda Security Group"
-}
-
-# S3 Bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "your-bucket-name"  # Replace with your desired bucket name
+# Create an S3 bucket
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "your-s3-bucket-name"  # Update with your desired bucket name
   acl    = "private"
-
-  versioning {
-    enabled = true
-  }
 }
 
-# Lambda Function
-resource "aws_lambda_function" "my_lambda_function" {
-  function_name    = "my_lambda_function"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "index.handler"
-  runtime          = "nodejs14.x"  # Change to your desired runtime
-  timeout          = 60
-  memory_size      = 256
-  source_code_hash = filebase64("${path.module}/lambda_function.zip")  # Zip your Lambda code
-  
-  environment = {
-    variables = {
-      S3_BUCKET = aws_s3_bucket.my_bucket.bucket
-    }
-  }
-}
-
-# EventBridge Rule
-resource "aws_cloudwatch_event_rule" "lambda_trigger" {
-  name        = "lambda_trigger_rule"
-  description = "Trigger Lambda every minute"
-  schedule_expression = "rate(1 minute)"
-}
-
-resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule = aws_cloudwatch_event_rule.lambda_trigger.name
-  arn  = aws_lambda_function.my_lambda_function.arn
-}
-
-# Allow Lambda to be triggered by EventBridge
-resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda_function.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.lambda_trigger.arn
+# Upload the YAML file to S3
+resource "aws_s3_bucket_object" "yaml_object" {
+  bucket = aws_s3_bucket.example_bucket.bucket
+  key    = "security_groups.yaml"
+  source = local_file.yaml_file.filename
 }
