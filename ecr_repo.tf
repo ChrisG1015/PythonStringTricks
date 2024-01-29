aae301b3ee051e148ef6a21de9c7832f4a4cfe9e
ChrisG1015 <45828554+ChrisG1015@users.noreply.github.com>
2024-01-29 11:26:33 -0500

    Create ecr_repo.tf

---
+++ ecr_repo.tf
@@ -0,0 +1,45 @@
+module "ecr_build" {
+  source            = "./ecr-module"
+  ecr_repo_name     = "your-ecr-repo-name"
+  dockerfile_path   = "../Dockerfile"  # Path relative to ecr-module directory
+}
+
+provider "aws" {
+  region = "your-aws-region"
+}
+
+resource "aws_ecr_repository" "this" {
+  name = var.ecr_repo_name
+}
+
+resource "null_resource" "build_push" {
+  provisioner "local-exec" {
+    command = "./scripts/build_push.sh ${aws_ecr_repository.this.repository_url} ${var.dockerfile_path}"
+  }
+}
+
+variable "ecr_repo_name" {
+  description = "Name for the ECR repository"
+  type        = string
+}
+
+variable "dockerfile_path" {
+  description = "Path to the Dockerfile"
+  type        = string
+}
+
+
+
+#!/bin/bash
+
+ecr_repo_url=$1
+dockerfile_path=$2
+
+# Build Docker image
+docker build -t $ecr_repo_url $dockerfile_path
+
+# Authenticate Docker to ECR
+aws ecr get-login-password --region your-aws-region | docker login --username AWS --password-stdin $ecr_repo_url
+
+# Push Docker image to ECR
+docker push $ecr_repo_url
