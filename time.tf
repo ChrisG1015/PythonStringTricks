66c8bcfca4f9c7012089e1bfa1622dd22307f45e
ChrisG1015 <45828554+ChrisG1015@users.noreply.github.com>
2024-07-24 20:28:49 -0400

    Update time.tf

--- time.tf
+++ time.tf
@@ -1,15 +1,1 @@
-variable "current_time" {}
-
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "date '+%Y-%m-%d-%H:%M' > current_time.txt"
-  }
-
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-}
-
-output "current_time" {
-  value = var.current_time
-}
+curl -X GET https://<databricks-instance>/api/2.0/serving-endpoints -H "Authorization: Bearer $DATABRICKS_API_TOKEN" -H "Content-Type: application/json"
